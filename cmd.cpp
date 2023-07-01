4d22d7311d5c562fea56b109062d58bbdd0573fe
WorthlessProgrammer <95526357+WorthlessProgrammer@users.noreply.github.com>
2023-07-01 12:29:34 +0200

    Update cmd.cpp

--- cmd.cpp
+++ cmd.cpp
@@ -2,22 +2,23 @@
 
 void cmd::ls(const char *dir_path)
 {
-    DIR *cwd;
-    if (dir_path == nullptr) cwd = opendir(".");
-    else cwd = opendir(dir_path);
+    DIR *target_directory;
+    target_directory = opendir(".");
+    //if (dir_path == nullptr) target_directory = opendir(".");
+    //else target_directory = opendir(dir_path);
 
-    if (cwd == nullptr) {
+    if (target_directory == nullptr) {
         std::cerr << "ERROR: Could not open directory \n";
         return;
     }
 
     struct dirent *entry = nullptr;
-    entry = readdir(cwd);
+    entry = readdir(target_directory);
     while (entry) {
         if (entry->d_name[0] != '.')
             std::cout << entry->d_name << "\n";
-        entry = readdir(cwd);
+        entry = readdir(target_directory);
     }
 
-    closedir(cwd);
+    closedir(target_directory);
 }

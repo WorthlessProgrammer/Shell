cc332a5a3e006a15aded2bf7d29d4e48f5b31b1e
WorthlessProgrammer <95526357+WorthlessProgrammer@users.noreply.github.com>
2023-07-01 12:37:13 +0200

    Add files via upload

---
+++ includes/cmd.h
@@ -0,0 +1,19 @@
+#ifndef CMD_H
+#define CMD_H
+
+// Includes
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+
+namespace cmd {
+
+void ls(const char *args);
+
+} // namespace cmd
+
+#endif //CMD_H

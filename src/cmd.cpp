1be36c417d4de7a8a9fff7338d26218eed36bd89
WorthlessProgrammer <95526357+WorthlessProgrammer@users.noreply.github.com>
2023-07-01 12:38:03 +0200

    Add files via upload

---
+++ src/cmd.cpp
@@ -0,0 +1,24 @@
+#include "cmd.h"
+
+void cmd::ls(const char *args)
+{
+    DIR *target_directory;
+    target_directory = opendir(".");
+    //if (dir_path == nullptr) target_directory = opendir(".");
+    //else target_directory = opendir(dir_path);
+
+    if (target_directory == nullptr) {
+        std::cerr << "ERROR: Could not open directory \n";
+        return;
+    }
+
+    struct dirent *entry = nullptr;
+    entry = readdir(target_directory);
+    while (entry) {
+        if (entry->d_name[0] != '.')
+            std::cout << entry->d_name << "\n";
+        entry = readdir(target_directory);
+    }
+
+    closedir(target_directory);
+}
